a0cc3929b853da77e4d66470a57a1a9cfc2c5070
Kassen <kassen.boyaubay@alumni.nu.edu.kz>
2025-08-14 23:39:58 +0500

    add ability to compile projects cpp files in task config

--- 001_setup/helloworld.cpp
+++ 001_setup/helloworld.cpp
@@ -8,16 +8,31 @@ void foo(int)
 {
 }
 
+int add(int, int);
+
 int main()
 {
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
+    cout << '\n';
     for (const string& word : msg)
     {
         cout << word << " ";
     }
-    cout << endl;
+    cout << '\n';
 
-    // unsigned int x { 5 };
+    cout << "add from add.cpp 2+3=" << add(2, 3) << '\n';
+
+    // attribute for compiler
+    // don't give warning if var is unused
+    // volatile = don't remove var in optimization process
+    [[maybe_unused]] unsigned int x { 5 }; // direct-list-initialization
     // foo(x); // should give warning and warning is treated as error
+
+    int t {}; // value-initialization
+    cout << "enter 1 value: " << '\n';
+    cin >> t; 
+    cout << "you entered " << t;
+    
+    cout << endl;
 }
